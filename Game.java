92ef67c4e445cd81f6f9c591f45019693c0d56f1
Jesse <jesse@jessejanssen.nl>
2023-10-05 13:15:05 +0200

    Further code

--- Game.java
+++ Game.java
@@ -1,9 +1,37 @@
+/**
+ * The game class.
+ */
 public class Game {
     public final int ticksPerSecond = 20;
 
-    private Field field;
+    public Field field;
+    public int gold;
 
     public void start() {
         this.field = new Field();
+    }
+
+    /**
+     * Buy a tower.
+     * 
+     * @param tower                     The tower to buy.
+     * @throws IllegalArgumentException Not enough gold or tower not placeable.
+     */
+    public void buyTower(Tower tower) throws IllegalArgumentException {
+        if (this.gold < tower.cost) {
+            throw new IllegalArgumentException("Not enough gold.");
+        }
+        this.field.addTower(tower);
+    }
+
+    /**
+     * Sell a tower.
+     * 
+     * @param tower                     The tower to sell.
+     * @throws IllegalArgumentException If the tower does not exist.
+     */
+    public void sellTower(Tower tower) throws IllegalArgumentException {
+        this.field.removeTower(tower);
+        this.gold += tower.getSellCost();
     }
 }
