d4189226d929cb775b0872e71b61e974213c2ef2
Jesse <jesse@jessejanssen.nl>
2023-10-09 15:12:30 +0200

    Fullscreen toggle

---
+++ src/gui/actions/FullscreenToggleAction.java
@@ -0,0 +1,41 @@
+package gui.actions;
+
+import gui.frame.Frame;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+/**
+ * The fullscreen toggle action.
+ */
+public class FullscreenToggleAction extends AbstractAction {
+    private Frame frame;
+    private GraphicsDevice device;
+
+    /**
+     * Constructor.
+     * 
+     * @param frame The frame.
+     */
+    public FullscreenToggleAction(Frame frame) {
+        this.frame = frame;
+        this.device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.frame.dispose();
+
+        if (frame.isUndecorated()) {
+            this.device.setFullScreenWindow(null);
+            this.frame.setUndecorated(false);
+        } else {
+            this.frame.setUndecorated(true);
+            this.device.setFullScreenWindow(this.frame);
+        }
+
+        this.frame.setVisible(true);
+        this.frame.repaint();
+    }
+}
--- src/gui/frame/Frame.java
+++ src/gui/frame/Frame.java
@@ -1,12 +1,15 @@
 package gui.frame;
 
 import game.Game;
+import gui.actions.FullscreenToggleAction;
 import gui.mouse.Mouse;
 import gui.panels.FieldPanel;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.Action;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
@@ -18,12 +21,13 @@ public class Frame extends BaseFrame {
     private Game game;
     private Mouse mouse;
 
-    private FieldPanel fieldPanel;
-
     public Frame(Game game) {
         this.game = game;
     }
 
+    /**
+     * Set the initial size of the frame.
+     */
     public void setInitialSize() {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double a = 0.6;
@@ -42,7 +46,7 @@ public class Frame extends BaseFrame {
         this.addMouseListener(mouse);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationByPlatform(true);
-        this.setupPanels();
+        this.setup();
         this.setVisible(true);
 
         ActionListener taskPerformer = new ActionListener() {
@@ -58,11 +62,27 @@ public class Frame extends BaseFrame {
     }
 
     /**
+     * Add a key binding.
+     * 
+     * @param key    The key.
+     * @param action The action.
+     */
+    public final void addKeyBinding(String key, Action action) {
+        JComponent c = (JComponent) this.getRootPane();
+        c.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+            javax.swing.KeyStroke.getKeyStroke(key),
+            key
+        );
+        c.getActionMap().put(key, action);
+        c.setFocusable(true);
+    }
+
+    /**
      * Setup the panels.
      */
-    private void setupPanels() {
-        this.fieldPanel = new FieldPanel(this, this.game);
-        this.add(this.fieldPanel);
+    private void setup() {
+        this.add(new FieldPanel(this, this.game));
+        this.addKeyBinding("F11", new FullscreenToggleAction(this));
     }
 
     public void tick() {
