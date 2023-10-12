f1a4ba8eee1c1991651c475748f6428f7e287e3a
Jesse <jesse@jessejanssen.nl>
2023-10-13 00:46:14 +0200

    Added fun python script

---
+++ src/total_lines.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+def main() -> None:
+    total_lines = 0
+    for path in Path('.').rglob('*.java'):
+        with path.open() as f:
+            total_lines += len(f.readlines())
+    print(f'Total lines of code: {total_lines}')
+
+
+if __name__ == '__main__':
+    main()
