cc801fa73af7fd2eeaf8e505ce9ca19553300f4f
Jesse <jesse@jessejanssen.nl>
2023-10-15 20:48:39 +0200

    Added easier labels

--- src/gui/painters/GoldLabelPainter.java
+++ src/gui/painters/GoldLabelPainter.java
@@ -6,6 +6,7 @@ import gui.Painter;
 import gui.Panel;
 import gui.frame.Frame;
 import javax.swing.JLabel;
+import java.awt.Color;
 
 
 /**
@@ -17,15 +18,20 @@ public final class GoldLabelPainter extends Painter {
      */
     public GoldLabelPainter(Game game, Frame frame, Panel panel) {
         super(game, frame, panel);
-        this.label = new JLabel();
-        this.label.setBounds(50, 50, 100, 50);
-        this.panel.add(label);
     }
 
-    private final JLabel label;
+    private String getText() {
+        return String.format("Gold: %d", this.game.getGold());
+    }
 
     @Override
     public void paint(BetterGraphics graphics) {
-        this.label.setText(String.format("%d gold", this.game.getGold()));
+        graphics.setColor(Color.WHITE);
+        graphics.setFont(2.5);
+        graphics.drawStringCenteredWithBox(
+            this.getText(),
+            this.game.field.width / 2, 3,
+            graphics.new Box(Color.BLACK, 0.5, 0.5)
+        );
     }
 }
