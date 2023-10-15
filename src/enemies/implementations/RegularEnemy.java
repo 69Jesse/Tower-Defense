e2bfc2367a2d7f23e033a5bcbf30df50dca45480
Jesse <jesse@jessejanssen.nl>
2023-10-15 23:32:39 +0200

    Create RegularEnemy.java

---
+++ src/enemies/implementations/RegularEnemy.java
@@ -0,0 +1,33 @@
+package enemies.implementations;
+
+import enemies.Enemy;
+import location.Location;
+
+
+/**
+ * A regular enemy.
+ */
+public class RegularEnemy extends Enemy {
+    /**
+     * Constructs a regular enemy.
+     * 
+     * @param location The location of this enemy on the field.
+     */
+    public RegularEnemy(Location location) {
+        super(
+            10,
+            1,
+            100,
+            1,
+            false,
+            location
+        );
+    }
+
+    @Override
+    public String getImagePath() {
+        return "./assets/enemies/regular_enemy.png";
+    }
+
+    
+}
