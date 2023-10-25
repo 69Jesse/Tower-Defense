2483a2e039e5825a73a4d71fdd9e6460e7ed081b
Jesse <jesse@jessejanssen.nl>
2023-10-25 20:58:44 +0200

    Added option for a seed

--- src/Launcher.java
+++ src/Launcher.java
@@ -7,6 +7,29 @@ import game.Game;
 public class Launcher {
     public static void main(String[] args) {
         Game game = new Game();
-        game.run(123L);
+        game.run(getSeed(args));
+    }
+
+    /**
+     * Returns the seed from the given arguments, or null if no seed was given.
+     * 
+     * @param args The arguments.
+     * @return     The seed from the given arguments, or null.
+     */
+    public static Long getSeed(String[] args) {
+        if (args.length == 0) {
+            return null;
+        }
+        if (args.length > 1) {
+            System.out.println("Too many arguments.");
+            System.exit(1);
+        }
+        try {
+            return Long.parseLong(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println(String.format("'%s' is not a valid seed.", args[0]));
+            System.exit(1);
+        }
+        return null;
     }
 }
