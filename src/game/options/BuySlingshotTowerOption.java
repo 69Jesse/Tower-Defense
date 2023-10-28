542a3e34f9de273f0b9dde451fec87d2550baf3c
Jesse <jesse@jessejanssen.nl>
2023-10-28 17:55:45 +0200

    Added  option

---
+++ src/game/options/BuySlingshotTowerOption.java
@@ -0,0 +1,43 @@
+package game.options;
+
+import field.Field;
+import game.Game;
+import location.Location;
+import towers.Tower;
+import towers.implementations.SlingshotTower;
+
+
+/**
+ * The buy slingshot tower option class.
+ */
+public class BuySlingshotTowerOption extends BuyTowerOption {
+    /**
+     * The constructor.
+     *
+     * @param game  The game.
+     * @param field The field.
+     */
+    public BuySlingshotTowerOption(
+        Game game,
+        Field field
+    ) {
+        super(game, field);
+    }
+
+    @Override
+    public Tower createTower(Location location) {
+        return new SlingshotTower(this.game, location);
+    }
+
+    @Override
+    public String getImagePath() {
+        return "./assets/options/laser_tower.png";
+    }
+
+    @Override
+    public String getLabel(Location location) {
+        return String.format(
+            "Buy Slingshot Tower (-%d gold)", this.getCost()
+        );
+    }
+}
